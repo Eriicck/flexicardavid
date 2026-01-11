3e905392c6d8a3969cbe0d599140c7084e035586
Eriicck <129214023+Eriicck@users.noreply.github.com>
2026-01-11 11:18:34 -0300

    numero corregido3

--- src/App.jsx
+++ src/App.jsx
@@ -535,7 +535,7 @@ const Footer = () => (
            </div>
            <div className="flex items-start gap-3">
                <Phone className="text-orange-500 shrink-0 mt-0.5" size={16} />
-               <p className="text-xs">+34 642 96 22</p>
+               <p className="text-xs">+34 642 02 96 62</p>
            </div>
         </div>
         {/* Horarios */}
